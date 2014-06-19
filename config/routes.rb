1b489d52dd06f1b62faba1a8853fa6adcf65168a
sskates1 <simonirv2@gmail.com>
2014-06-19 14:32:18 -0400

    Added index page for characters and created footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 TvShows::Application.routes.draw do
   resources :television_shows, only: [:index, :show, :new, :create] do
-    resources :characters, only: [:index, :create, :destroy]
+    resources :characters, only: [ :create, :destroy]
   end
+
+  resources :characters, only: [:index]
 
   get '/', to: 'television_shows#index'
 end
